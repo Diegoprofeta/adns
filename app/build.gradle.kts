2643fd09394ac449f47226baeb5351ac7fe177e4
Eyal <141010573+eyalm2000@users.noreply.github.com>
2026-07-25 19:44:48 +0300

    Bump to 2.0.0-beta3.2 and update app descriptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.eyalm.adns"
         minSdk = 27
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0.0-beta3.1"
+        versionCode = 12
+        versionName = "2.0.0-beta3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
