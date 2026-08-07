14aeaa4d4d0c0374c4776cb14eadf8a8321a599b
Eyal <141010573+eyalm2000@users.noreply.github.com>
2026-08-07 15:24:57 +0300

    Bump to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.eyalm.adns"
         minSdk = 27
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.0.0"
+        versionCode = 14
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
